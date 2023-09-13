122e83688b5bfe81e1d5742819f1c4722cb49c62
sukritpant <sukritpant@gmail.com>
2023-09-13 11:32:58 +0300

    Updated the code to Python 3

--- RenameAcademicPDFAccordingToItsMetadata.py
+++ RenameAcademicPDFAccordingToItsMetadata.py
@@ -27,30 +27,31 @@ import unicodedata
 
 validFilenameChars = "-_.() %s%s" % (string.ascii_letters, string.digits)
 
+
 def removeDisallowedFilenameChars(filename):
-    cleanedFilename = unicodedata.normalize('NFKD', unicode(filename)).encode('ASCII', 'ignore')
+    cleanedFilename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore').decode('ASCII')
     return ''.join(c for c in cleanedFilename if c in validFilenameChars)
 
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+print ("Number of arguments:", len(sys.argv), "arguments.")
+print ('Argument List:', str(sys.argv))
 
 if len(sys.argv) != 2:
-    print "Wrong number of arguments --- Abort"
+    print ("Wrong number of arguments --- Abort")
     exit()
 
 filename = sys.argv[1]
     
 if not os.path.exists(filename):
-    print  filename+" does not exist--- Abort"
+    print  (filename+" does not exist--- Abort")
     exit()
 
 
 try:
     pdfrw_info= PdfReader(filename).Info
-    print filename+" its as readable pdf file"
+    print (filename+" its as readable pdf file")
 except:
-    print  "Unable to read pdf with pdfrw "+filename+" --- WARNING"
+    print  ("Unable to read pdf with pdfrw "+filename+" --- WARNING")
     pdfrw_info = None
     #exit()
 
@@ -58,8 +59,8 @@ except:
 if pdfrw_info != None: 
 
     # Info from pdfrw
-    print "\n\tInformation retrieved by pdfrw:"
-    print "\t",pdfrw_info
+    print ("\n\tInformation retrieved by pdfrw:")
+    print ("\t",pdfrw_info)
 
 
 # info from pypdf2
@@ -67,7 +68,7 @@ if pdfrw_info != None:
 try:
     ipdf = PdfFileReader(open(filename, 'rb'))
 except:
-    print  "Unable to read pdf with pypdf2 "+filename+" --- Warning"
+    print  ("Unable to read pdf with pypdf2 "+filename+" --- Warning")
     ipdf = None 
     #exit()
 
@@ -75,10 +76,10 @@ except:
 pypdf2_info=ipdf.getDocumentInfo()
 pypdf2_xmp=ipdf.getXmpMetadata()
 
-print "\n\tInformation retrieved by pypdf2:"
-print "\t", pypdf2_info
-print "\n\tXMP Information retrieved by pypdf2:"
-print "\t",pypdf2_xmp
+print ("\n\tInformation retrieved by pypdf2:")
+print ("\t", pypdf2_info)
+print ("\n\tXMP Information retrieved by pypdf2:")
+print ("\t",pypdf2_xmp)
 
 
 # Save now the author, year, title, publisher information if possible
@@ -120,7 +121,7 @@ for key, value in pypdf2_info.items():
     if 'date' in key.lower():
         date_str = value 
 
-print "\n\t pdfrw or pypdf2 date_str=", date_str
+print ("\n\t pdfrw or pypdf2 date_str=", date_str)
         
 # For now we are able to get the date if signed in the fortat \dyyyy
 
@@ -142,7 +143,7 @@ def getYearFromDateStr(date_str):
     return "Unable to process year information" 
 
 
-print "\n\t year retrived from date string =", getYearFromDateStr(date_str)
+print ("\n\t year retrived from date string =", getYearFromDateStr(date_str))
 
 year = getYearFromDateStr(date_str)
         
@@ -177,7 +178,7 @@ if pypdf2_xmp != None :
         for key, value in pypdf2_xmp.dc_title.items():
             title = title + value
 
-    print "\n\t title retrieved by pypdf2_xmp.dc_title=",title 
+    print ("\n\t title retrieved by pypdf2_xmp.dc_title=",title )
         
 # attempt now the publication name
 publication = "Unkown publication"
@@ -217,7 +218,7 @@ for key, value in pypdf2_info.items():
 if pypdf2_xmp != None :
     if pypdf2_xmp.dc_source != None and pypdf2_xmp.dc_source != {}:
         publication = pypdf2_xmp.dc_source
-    print "\n\t publication retrieved by pypdf2_xmp.dc_title=",publication
+    print ("\n\t publication retrieved by pypdf2_xmp.dc_title=",publication)
 
 
 
@@ -246,14 +247,14 @@ for key, value in pypdf2_info.items():
 
 
 
-print ""
-print " \tRetrieved METADATA *********************" 
-print '\tauthor='+author
-print '\tyear='+year
-print '\ttitle='+title
-print '\tpublication='+publication
-print '\tpublisher='+publisher
-print "\t*******************************************\n\n" 
+print ("")
+print (" \tRetrieved METADATA *********************" )
+print ('\tauthor='+author)
+print ('\tyear='+year)
+print ('\ttitle='+title)
+print ('\tpublication='+publication)
+print ('\tpublisher='+publisher)
+print ("\t*******************************************\n\n" )
 
 
 # Trying now to get publication from the file itself
@@ -261,80 +262,80 @@ print "\t*******************************************\n\n"
 "find a pattern in a give file" 
 def findPatternInFile(file,pattern):
     cmd = '/usr/bin/strings "'+ filename +'"  | /bin/grep publication'
-    print "invoking", cmd
+    print ("invoking", cmd)
 
     try:
         res_cmd = subprocess.check_output(cmd, shell=True)
     except subprocess.CalledProcessError as e:
-        print e
+        print (e)
 
         if e.output.startswith('error: {'):
             error = json.loads(e.output[7:]) # Skip "error: "
-            print error['code']
-            print error['message']
+            print (error['code'])
+            print (error['message'])
     
         return False 
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print ("Unexpected error:", sys.exc_info()[0])
         return False 
 
         
-    print "\t\t DEBUG LEVEL 2 - strings + grep returned ", res_cmd
+    print ("\t\t DEBUG LEVEL 2 - strings + grep returned ", res_cmd)
 
         
     # looking for <prism:publicationName>The Journal of Academic Librarianship</prism:publicationName>
     m = re.findall(pattern, res_cmd)
 
-    print "\t\t DEBUG LEVEL 2 - First found patter m[0]=", m[0]
+    print ("\t\t DEBUG LEVEL 2 - First found patter m[0]=", m[0])
 
     if m != None:
         return  m[0]
     else:
-        print "error can find publication name"
+        print ("error can find publication name")
         return False 
 
     
     
 if publication  == "Unkown publication":
-    print "Trying now to get publication from the file itself"
+    print ("Trying now to get publication from the file itself")
 
     foundPublicationName = findPatternInFile(filename,'<prism:publicationName>(.*)</prism:publicationName>')
 
-    print "\t \t DEBUG LEVEL 1 - foundPublicationName in file =", foundPublicationName 
+    print ("\t \t DEBUG LEVEL 1 - foundPublicationName in file =", foundPublicationName )
     
     if foundPublicationName != False:
         publication =  foundPublicationName
     
 
-        print " \tRetrieved from file  *********************" 
-        print '\tpublication='+publication
-        print " \t******************************************" 
+        print (" \tRetrieved from file  *********************" )
+        print ('\tpublication='+publication)
+        print (" \t******************************************" )
 
 # Tries also to get title using docear (see http://www.docear.org/software/add-ons/docears-pdf-inspector/)
 # calls java -jar docears-pdf-inspector/docears-pdf-inspector.jar  -title 
 
 def getDocearTitle(filename):
-    print "\n\t DEBUG 1 Attempting to get title from docear"
+    print ("\n\t DEBUG 1 Attempting to get title from docear")
 
     cmd = '/usr/bin/java  -jar /home/apolinex/ownCloud/FLOSS-dev/renamePdfByMetaData/renameacademicpdfaccordingtoitsmetadata/docears-pdf-inspector/docears-pdf-inspector.jar  -title "'+ filename + '"'
-    print "invoking", cmd
+    print ("invoking", cmd)
 
     try:
         res_cmd = subprocess.check_output(cmd, shell=True)
     except subprocess.CalledProcessError as e:
-        print e
+        print (e)
 
         if e.output.startswith('error: {'):
             error = json.loads(e.output[7:]) # Skip "error: "
-            print error['code']
-            print error['message']
+            print (error['code'])
+            print (error['message'])
     
         return False 
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print ("Unexpected error:", sys.exc_info()[0])
         return False 
 
-    print "\n\t DEBUG 1 docear return title:", res_cmd 
+    print ("\n\t DEBUG 1 docear return title:", res_cmd )
 
     if res_cmd != None:
         return res_cmd
@@ -355,28 +356,28 @@ if (title=="Unknown title"  or title == '' or title ==' ' ) and publisher != 'El
 
 
 def  get_filename_prefix_str(filepath):
-    print "\n\t\t DEBUG LEVEL 2 finding filename prefix for filepath =[",filepath,"]"
+    print ("\n\t\t DEBUG LEVEL 2 finding filename prefix for filepath =[",filepath,"]")
 
     # Deals with files that were alreadt renamed by this software 
     filename_prefix_index= filepath.find("(only able to retrieve")
     if filename_prefix_index > 1:
-        print "Warning: it was already renamed"
+        print ("Warning: it was already renamed")
         return  filepath[:filename_prefix_index] 
 
     filename_prefix_index= filepath.find("(unable to retrieve any meta-data)")
     if filename_prefix_index > 1:
-        print "Warning: it was already renamed"
+        print ("Warning: it was already renamed")
         return  filepath[:filename_prefix_index] 
 
     # Otherwise returm the string until the first .pdf
     filename_prefix_index= filepath.find(".pdf")
     
     if filename_prefix_index > 1:
-        print "\n\t\t DEBUG LEVEL 2 filename prefix = ", filepath[:filename_prefix_index] 
+        print ("\n\t\t DEBUG LEVEL 2 filename prefix = ", filepath[:filename_prefix_index] )
         return  filepath[:filename_prefix_index] 
     
     else:
-        print "Extension error: is it name .pdf?"
+        print ("Extension error: is it name .pdf?")
         sys.exit()
 
     
@@ -384,29 +385,29 @@ def  get_filename_prefix_str(filepath):
 ## Nothing is known 
 if author=='Unknown author' and year=='Unknown'  and title=='Unknown title' and publication=='Unkown publication' and publisher=='Unkown publisher':
     newname = get_filename_prefix_str(filename)  + "(unable to retrieve any meta-data).pdf"
-    print "\t\n--Unable--"
+    print ("\t\n--Unable--")
 
 ## Only one field is known 
 elif author=='Unknown author' and year!='Unknown'  and title=='Unknown title' and publication=='Unkown publication' and publisher=='Unkown publisher':
     newname = get_filename_prefix_str(filename)  + "(only able to retrieve year from  meta-data).pdf"
-    print "\t\n--Able to get year only--"
+    print ("\t\n--Able to get year only--")
 
 elif author!='Unknown author' and year=='Unknown'  and title=='Unknown title' and publication=='Unkown publication' and publisher=='Unkown publisher':
     newname = get_filename_prefix_str(filename) + "(only able to retrieve author from  meta-data).pdf"
-    print "\t\n--Able to get author only--"
+    print ("\t\n--Able to get author only--")
 
 elif author=='Unknown author' and year=='Unknown'  and title!='Unknown title' and publication=='Unkown publication' and publisher=='Unkown publisher':
     newname = get_filename_prefix_str(filename)  + "(only able to retrieve title from meta-data).pdf"
-    print "\t\n--Able to get title only--"
+    print ("\t\n--Able to get title only--")
     
 
 elif author=='Unknown author' and year=='Unknown'  and title=='Unknown title' and publication!='Unkown publication' and publisher=='Unkown publisher':
     newname = get_filename_prefix_str(filename)  + "(only able to retrieve publication from meta-data).pdf"
-    print "\t\n--Able to get publication only--"
+    print ("\t\n--Able to get publication only--")
 
 elif author=='Unknown author' and year=='Unknown'  and title=='Unknown title' and publication=='Unkown publication' and publisher!='Unkown publisher':
     newname = get_filename_prefix_str(filename)  + "(only able to retrieve publisher from meta-data).pdf"
-    print "\t\n--Able to get publisher only--"
+    print ("\t\n--Able to get publisher only--")
 
 
 ## If at least author and year is known but not the title -> use the filename as differenciator 
@@ -418,7 +419,7 @@ elif author!='Unknown author' and year!='Unknown' and title=='Unknown title':
     str_prefix_tmp = str_prefix_tmp[:15]
     
     newname = author + " " + year + " " + "(unknown title) ["+  str_prefix_tmp +"]" + " @" + publication + ". " + publisher + ".pdf"
-    print "\t\n--Able to get at least author and year--"
+    print ("\t\n--Able to get at least author and year--")
 
 
 
@@ -426,7 +427,7 @@ elif author!='Unknown author' and year!='Unknown' and title=='Unknown title':
 
 elif title!='Unknown title' :
     newname = os.path.dirname(filename) + "/" +  author + " (" + year + ") " + title  + " @" + publication + ". " + publisher + ".pdf"
-    print "\t\n--Able to get at least get the author  year--"
+    print ("\t\n--Able to get at least get the author  year--")
 
 
 ## Able to get all information
@@ -434,34 +435,34 @@ elif title!='Unknown title' :
 elif author!='Unknown author' and year!='Unknown'  and title!='Unknown title' and publication!='Unkown publication' and publisher!='Unkown publisher':
 
     newname = os.path.dirname(filename)  + "/"  + author + " (" + year + ") " + title  + " @" + publication + ". " + publisher + ".pdf"
-    print "\t\n--Able to get all necessary metadata--"
+    print ("\t\n--Able to get all necessary metadata--")
 
 else:
-    print "ERROR, unable to get necessart information to perform an remanme"
+    print ("ERROR, unable to get necessart information to perform an remanme")
     sys.exit(-1)
 
 # Validate the new file name
 
 
-print "\n\t DEBUG LEVEL 1 - oldFileName ["+ filename+"]"
-print "\n\t DEBUG LEVEL 1 - newName before validation["+ newname+"]"
+print ("\n\t DEBUG LEVEL 1 - oldFileName ["+ filename+"]")
+print ("\n\t DEBUG LEVEL 1 - newName before validation["+ newname+"]")
 
 newFileNameBase= newname[len(os.path.dirname(filename)):]
 
 # Remove the / that apears often in doi 
-print "\n\t DEBUG LEVEL 1 - newFileNameBase (newName before validation) before removing /: ["+newFileNameBase+"]"
+print ("\n\t DEBUG LEVEL 1 - newFileNameBase (newName before validation) before removing /: ["+newFileNameBase+"]")
 
 newFileNameBase = newFileNameBase.replace('/',' ')
-print "\n\t DEBUG LEVEL 1 - newFileNameBase (newName before validation)  after removing /: ["+newFileNameBase+"]"
+print ("\n\t DEBUG LEVEL 1 - newFileNameBase (newName before validation)  after removing /: ["+newFileNameBase+"]")
 
 newFileNameBase= removeDisallowedFilenameChars(newFileNameBase)
-print "\n\t DEBUG LEVEL 1 - newFileNameBase  after removing disallowed charts            : [" +newFileNameBase+"]"
+print ("\n\t DEBUG LEVEL 1 - newFileNameBase  after removing disallowed charts            : [" +newFileNameBase+"]")
 
 newFileNameBase=newFileNameBase.lstrip(' ')
 newFileNameBase=newFileNameBase.lstrip('\t')
 newFileNameBase=newFileNameBase.lstrip('\n')
 
-print "\n\t DEBUG LEVEL 1 - newFileNameBase  after removing ' ', '\n', '\t'              : [" +newFileNameBase+"]"
+print ("\n\t DEBUG LEVEL 1 - newFileNameBase  after removing ' ', '\n', '\t'              : [" +newFileNameBase+"]")
 
 
 
@@ -471,24 +472,24 @@ print "\n\t DEBUG LEVEL 1 - newFileNameBase  after removing ' ', '\n', '\t'     
 # Not implemented yet
 
 if len(newFileNameBase) > 200:
-    print "\n WARNING - newFileNameBase name longer that 200 char"
+    print ("\n WARNING - newFileNameBase name longer that 200 char")
     pre = newFileNameBase[:100]
     suf = newFileNameBase[-95:]
-    print "pre=", pre 
-    print "suf=", suf
+    print ("pre=", pre )
+    print ("suf=", suf )
     newFileNameBase =  pre + "..." +  suf
 
-print "\n\t DEBUG LEVEL 1 - newFileNameBase  after resize                                :["+newFileNameBase+"]"
+print ("\n\t DEBUG LEVEL 1 - newFileNameBase  after resize                                :["+newFileNameBase+"]")
 
 
 newname= os.path.join(os.path.dirname(filename),newFileNameBase)
-print "\n\t DEBUG LEVEL 1 - final new file name =", newname
+print ("\n\t DEBUG LEVEL 1 - final new file name =", newname)
 
 os.rename(filename,newname)        
-print "\n\tRES: ",filename," renamed to ", newname
+print ("\n\tRES: ",filename," renamed to ", newname)
     
 ## Title and something else 
     
-print 'END' 
+print ('END' )
 
 
