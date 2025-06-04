4862f16364472e1a568a96e5cb5abb78e8968cce
Jose Teixeira <japolinario@gmail.com>
2025-06-04 23:08:39 +0300

    improved the logger

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,10 @@
 sudo apt-get install python3-rich
 sudo apt-get install python3-loguru
 sudo apt-get install python3-fitz
-
 sudo apt-get install python3-pip
 
 pip3 install habanero --break-system packages
 pip3 install blosc2 --break-system-package
 pip3 install transformers --break-system packages
 pip3 install torch --break-system packages
-
-
-
+pip3 install varname --break-system packages
